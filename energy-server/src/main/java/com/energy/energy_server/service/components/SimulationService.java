8e740f0daba87241557fce4920dfc68a4befb94a
nameisalfio <alfio.spoto@outlook.it>
2025-12-18 18:05:42 +0100

    feat(Circuit Breaker) Implemented Circuit Breaking aspects

--- energy-server/src/main/java/com/energy/energy_server/service/components/SimulationService.java
+++ energy-server/src/main/java/com/energy/energy_server/service/components/SimulationService.java
@@ -2,7 +2,7 @@ package com.energy.energy_server.service.components;
 
 import java.time.LocalDateTime;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentLinkedQueue; 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.springframework.stereotype.Service;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.energy.energy_server.model.EnergyReading;
 import com.energy.energy_server.repository.EnergyRepository;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,9 +37,17 @@ public class SimulationService {
         isRunning.set(false);
     }
 
+    // --- CIRCUIT BREAKER ---
+    @CircuitBreaker(name = "energyDbBreaker", fallbackMethod = "fallbackSave")
     public void saveReading(EnergyReading entity) {
         entity.setTimestamp(LocalDateTime.now());
         repository.save(entity);
+    }
+
+    // --- METODO DI FALLBACK ---
+    public void fallbackSave(EnergyReading entity, Throwable t) {
+        log.warn("DATABASE STRESS: Circuit Open! Skipping save for record. Error: {}", t.getMessage());
+        // Here we could implement the RabbitMQ logging logic
     }
     
     public long getRecordCount() {
