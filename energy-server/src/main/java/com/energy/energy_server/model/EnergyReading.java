69ae17f827d2638a9432a15300cfee29acaa4d19
GabrieleRuggieri <gabri20022002@gmail.com>
2026-01-18 18:36:33 +0100

    new field and index correlation id

--- energy-server/src/main/java/com/energy/energy_server/model/EnergyReading.java
+++ energy-server/src/main/java/com/energy/energy_server/model/EnergyReading.java
@@ -2,18 +2,16 @@ package com.energy.energy_server.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Table(name = "energy_readings")
+@Table(name = "energy_readings", indexes = {
+        @Index(name = "idx_correlation_id", columnList = "correlationId", unique = true)
+})
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnergyReading {
@@ -21,6 +19,9 @@ public class EnergyReading {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String correlationId;
 
     private LocalDateTime timestamp;
     private Double temperature;
