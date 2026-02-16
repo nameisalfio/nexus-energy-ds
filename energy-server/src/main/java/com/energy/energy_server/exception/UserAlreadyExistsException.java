1f3fc3747d64f4cdb5dea1f014253a67b8a2412e
GabrieleRuggieri <gabri20022002@gmail.com>
2026-02-16 23:10:46 +0100

    new exception

---
+++ energy-server/src/main/java/com/energy/energy_server/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.energy.energy_server.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
